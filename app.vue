d7dee6b5bcaa85460c3929ef90f7bea286111c2e
Andrei Ujica <andrei@vipra.tech>
2024-07-19 14:09:44 +0300

    feat: install DM Sans font

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <Header />
-  <main class="bg-white/75 dark:bg-gray-900/75 min-h-[calc(100vh-4rem)]">
-    lala
+  <main
+    class="bg-white/75 dark:bg-gray-900/75 min-h-[calc(100vh-4rem)] mx-auto px-20 sm:px-28 lg:px-36 pt-8"
+  >
+    <NewRecipeCard />
   </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxt/test-utils/module"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@nuxt/test-utils/module",
+    "@nuxt/fonts",
+  ],
   ui: {
     icons: ["tabler"],
   },
+  css: ["~/assets/css/main.css"],
 });
