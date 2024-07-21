316c35fe269916ba1839fcef1c2ca7feb296240d
Andrei Ujica <andrei@vipra.tech>
2024-07-21 20:35:37 +0300

    fix: fix broken shoppingList test

--- tests/stores/shoppingList.test.ts
+++ tests/stores/shoppingList.test.ts
@@ -1,13 +1,20 @@
 import { describe, test, expect, beforeEach } from "vitest";
 import { setActivePinia, createPinia } from "pinia";
 import { useShoppingListStore } from "~/stores/shoppingList";
+import { useIngredientStore } from "~/stores/ingredient";
+import { useRecipeStore } from "~/stores/recipe";
 
 describe("Shopping List store", () => {
   let shoppingListStore = useShoppingListStore();
+  let ingredientsStore = useIngredientStore();
+  let recipeStore = useRecipeStore();
 
   beforeEach(() => {
     setActivePinia(createPinia());
     shoppingListStore = useShoppingListStore();
+
+    ingredientsStore = useIngredientStore();
+    recipeStore = useRecipeStore();
   });
 
   test("getShoppingList returns an empty array initially", () => {
@@ -20,7 +27,7 @@ describe("Shopping List store", () => {
     shoppingListStore.addRecipe(recipeId);
 
     const shoppingList = shoppingListStore.getShoppingList;
-    expect(shoppingList).toEqual([[recipeId, 1]]);
+    expect(shoppingList).toEqual([[recipeStore.getRecipeById(recipeId), 1]]);
   });
 
   test("addRecipe increments the count if the recipe is already in the shopping list", () => {
@@ -29,7 +36,7 @@ describe("Shopping List store", () => {
     shoppingListStore.addRecipe(recipeId);
 
     const shoppingList = shoppingListStore.getShoppingList;
-    expect(shoppingList).toEqual([[recipeId, 2]]);
+    expect(shoppingList).toEqual([[recipeStore.getRecipeById(recipeId), 2]]);
   });
 
   test("removeRecipe decrements the count if the recipe count is more than 1", () => {
@@ -39,7 +46,7 @@ describe("Shopping List store", () => {
     shoppingListStore.removeRecipe(recipeId);
 
     const shoppingList = shoppingListStore.getShoppingList;
-    expect(shoppingList).toEqual([[recipeId, 1]]);
+    expect(shoppingList).toEqual([[recipeStore.getRecipeById(recipeId), 1]]);
   });
 
   test("removeRecipe removes the recipe if the count is 1", () => {
@@ -61,5 +68,21 @@ describe("Shopping List store", () => {
 
     const shoppingList = shoppingListStore.getShoppingList;
     expect(shoppingList).toEqual([]);
+  });
+
+  test("getIngredientTotals calculates the correct ingredient totals", () => {
+    shoppingListStore.addRecipe(1);
+    shoppingListStore.addRecipe(2);
+
+    const ingredientTotals = shoppingListStore.getIngredientTotals;
+
+    expect(ingredientTotals).toEqual([
+      { ingredientId: 1, quantity: 300, usedUnit: "g" },
+      { ingredientId: 2, quantity: 200, usedUnit: "g" },
+      { ingredientId: 3, quantity: 7, usedUnit: "piece" },
+      { ingredientId: 4, quantity: 100, usedUnit: "g" },
+      { ingredientId: 6, quantity: 150, usedUnit: "g" },
+      { ingredientId: 5, quantity: 100, usedUnit: "g" },
+    ]);
   });
 });
